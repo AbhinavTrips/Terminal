b58ccfc6cf97dcdb0286efba98df848c07b28e9c
AbhinavDAIICT <abhinavtripathi01@hotmail.com>
2015-07-18 18:15:31 +0530

    added command history feature
    Now command history can be used using up and down arrow keys, just like
    the Unix terminal

---
+++ keyPress.js
@@ -0,0 +1,37 @@
+﻿
+var arr = new Array(500);
+arr[0] = 0;
+var tailIndex = 1;
+var currentPointer = tailIndex;
+document.onkeydown = keyPressed;
+
+function keyPressed(e) {
+
+    e = e || window.event;
+
+    if (e.keyCode == '38') {
+    	// up arrow
+    	if(currentPointer > 1){
+    	currentPointer--;
+    	document.getElementById("user-caption").value = arr[currentPointer];
+        }
+        }
+    
+    else if (e.keyCode == '40') {
+    	//down arrow
+    	if(currentPointer<tailIndex-1){
+    	currentPointer++;
+    	document.getElementById("user-caption").value = arr[currentPointer];
+    	
+    	}
+    }
+
+    else if (e.keyCode == '13') {
+    	// enter key
+    	arr[tailIndex] = $('input#user-caption').val();
+    	tailIndex++;
+    	currentPointer = tailIndex;
+    	arr[0]++;
+    }
+}
+
